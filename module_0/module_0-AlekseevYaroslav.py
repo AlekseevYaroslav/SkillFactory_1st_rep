cf9a46c5303a941efcc38a0f83673d8184c1ff72
AlekseevYaroslav <66815290+AlekseevYaroslav@users.noreply.github.com>
2020-06-13 13:22:21 +0300

    Optimization of the game

---
+++ module_0/module_0-AlekseevYaroslav.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def game_core_v3(number):
+    """Поиск загаданного числа методом золотого сечения"""
+    count = 0
+    x1 = 1
+    x2 = 101
+    predict = None
+    while number != predict:
+        count += 1
+        predict = (x1 + x2) // 2
+        if number > predict:
+            x1 = predict
+        else:
+            x2 = predict
+    return count
+
+
+def score_game(game_core):
+    """Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число"""
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=1000)
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return score
+
+
+number = np.random.randint(1, 101)  # загадали число
+
+score_game(game_core_v3)
